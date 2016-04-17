c680e3a76c77287d1fd45fc88af4f40e8451f1ad
Gustavo Rodrigues <Gustavo-Rodrigues@users.noreply.github.com>
2016-04-17 01:21:23 -0300

    Delete SimulatedAnnealing.java

--- SimulatedAnnealing.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-import java.util.Collections;
-
-public interface SimulatedAnnealing{
-    public double acceptanceProbability(int energy, int newEnergy, double temperature);
-    public void simulatedAnnealing(Set set);
-}
